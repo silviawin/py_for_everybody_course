4a5d301b05834312a9dfd915e1e72e1427d6622e
Silvia Winetzki <silviawinetzki@gmail.com>
2021-11-01 00:27:40 -0300

    Added conditional_steps.py file
    Examples of conditionals

---
+++ conditional_steps.py
@@ -0,0 +1,24 @@
+x = int(input("Type a number: "))
+if x < 10:
+    print("Smaller")
+if x > 20:
+    print("Bigger")
+print("That´s it")
+print("\n")
+if x == 5:
+    print('Equals 5')
+if x > 4:
+    print('Greater than 4')
+if x >= 5:
+    print('Greater than or equals to 5')
+if x < 6:
+    print('Less than 6')
+if x <= 5:
+    print('Less than or Equals to 5')
+if x != 6:
+    print('Not equal 6')
+
+    
+
+
+
