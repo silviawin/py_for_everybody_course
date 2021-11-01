f06df7a55afb23d769690606c816ab49399ee4f3
Silvia Winetzki <silviawinetzki@gmail.com>
2021-11-01 00:30:19 -0300

    Added file Remarks.py
    Just commented notes

---
+++ Remarks.py
@@ -0,0 +1,46 @@
+# reserved words
+# Python
+# Keywords
+# False	def	if	raise
+# None	del	import	return
+# True	elif	in	try
+# and	else	is	while
+# as	except	lambda	with
+# assert	finally	nonlocal	yield
+# break	for	not
+# class	from	or
+# continue	global	pass
+#
+#
+# variable naming rules:
+# começa com letra ou underscore ( não numero nem simbolo)  # starting with underscore geralm// pra se comunicar com python/sistema
+# consiste de letras, numeros e underscore
+# case sensitive
+#
+# there is ( variable, operator, constant, reserved word)
+# x = 2 assignent statement
+# x = x + 2 assignement with expression
+# print(x) print statement
+#
+# math operators +,-,*,/, ** ( power) % remainder
+#
+# math order of precedence rules: parentheses, exponentiation, mult/div/remainder, add/sub, left to right
+#
+# type function: pra saber o data type de um elemento
+# type(1)
+# class'int'
+# type(1.0)
+# class'float
+#
+# Conversions:
+# Floats: normally the number is implicitly converted to a float. If you want to change it there are the type conversions:
+# int()
+# float()
+# Division: From python 3, the integer division produces floating point number results
+# Use int() and float() to convert from strings ( error if string does not contain numbers)
+#
+# Input function:
+# #python pause and read data from user, returnas a string
+# nam = input('Who are you?')
+# print('Welcome', nam)
+#
