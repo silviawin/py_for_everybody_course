bcf64f21fd38150ad2c98c92922bcd95800d826b
Silvia Winetzki <silviawinetzki@gmail.com>
2021-11-01 00:29:31 -0300

    Added convert_input.py file
    Exercise about updating variable

---
+++ convert_input.py
@@ -0,0 +1,5 @@
+# convert Europe floor to American floor in builing's elevators convention
+
+europe_convention = input('What floor do you want to go: ')
+american_convention = int(europe_convention) + 1
+print("This floor would be: ", american_convention, "in the US")
